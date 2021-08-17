adb2c1122d32f2a870d892177246bfb29da4d976
Vladislav <umpkf1234@gmail.com>
2021-08-17 17:52:21 +0300

    Removed logic from the controller to the service

--- src/main/java/com/example/demohyper/controller/RecipeController.java
+++ src/main/java/com/example/demohyper/controller/RecipeController.java
@@ -3,10 +3,8 @@ package com.example.demohyper.controller;
 
 import com.example.demohyper.model.Recipe;
 import com.example.demohyper.service.RecipeService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 
 import javax.validation.Valid;
@@ -28,36 +26,18 @@ class RecipeController {
     }
 
     @GetMapping("api/recipe/{id}")
-    public Recipe getRecipe(@PathVariable int id) {
-        Optional<Recipe> opt = Optional.ofNullable(recipeService.findRecipeByID(id));
-        if (opt.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        } else {
-            return recipeService.findRecipeByID(id);
-        }
+    public ResponseEntity<Recipe> getRecipe(@PathVariable int id) {
+        return recipeService.findRecipe(id);
     }
 
     @DeleteMapping("api/recipe/{id}")
     public ResponseEntity deleteRecipe(@PathVariable int id) {
-        Optional<Recipe> opt = Optional.ofNullable(recipeService.findRecipeByID(id));
-        if (opt.isPresent()) {
-            recipeService.deleteRecipeByID(id);
-            return new ResponseEntity(HttpStatus.NO_CONTENT);
-        } else {
-            return new ResponseEntity(HttpStatus.NOT_FOUND);
-        }
+        return recipeService.deleteRecipe(id);
     }
 
     @PutMapping("api/recipe/{id}")
     public ResponseEntity updateRecipe(@PathVariable int id, @Valid @RequestBody Recipe newRecipe) {
-        Optional<Recipe> optionalRecipe = Optional.ofNullable(recipeService.findRecipeByID(id));
-        if (optionalRecipe.isPresent()) {
-            newRecipe.setId(id);
-            recipeService.save(newRecipe);
-            return new ResponseEntity(HttpStatus.NO_CONTENT);
-        } else {
-            return new ResponseEntity(HttpStatus.NOT_FOUND);
-        }
+        return recipeService.updateRecipe(id, newRecipe);
     }
 
     @GetMapping(value = "api/recipe/search/", params = "category")
--- src/main/java/com/example/demohyper/service/RecipeService.java
+++ src/main/java/com/example/demohyper/service/RecipeService.java
@@ -3,9 +3,13 @@ package com.example.demohyper.service;
 import com.example.demohyper.model.Recipe;
 import com.example.demohyper.repository.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RecipeService {
@@ -16,16 +20,38 @@ public class RecipeService {
         this.recipeRepository = recipeRepository;
     }
 
-    public Recipe findRecipeByID(int id) {
-        return recipeRepository.findRecipeById(id);
+    public ResponseEntity<Recipe> findRecipe(int id) {
+        Optional<Recipe> opt = Optional.ofNullable(recipeRepository.findRecipeById(id));
+        if (opt.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        } else {
+            return ResponseEntity.ok().body(opt.get());
+        }
     }
 
     public Recipe save(Recipe toSave) {
         return recipeRepository.save(toSave);
     }
 
-    public void deleteRecipeByID(int id) {
-        recipeRepository.deleteById(id);
+    public ResponseEntity deleteRecipe(int id) {
+        Optional<Recipe> opt = Optional.ofNullable(recipeRepository.findRecipeById(id));
+        if (opt.isPresent()) {
+            recipeRepository.deleteById(id);
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    public ResponseEntity updateRecipe(int id, Recipe newRecipe) {
+        Optional<Recipe> optionalRecipe = Optional.ofNullable(recipeRepository.findRecipeById(id));
+        if (optionalRecipe.isPresent()) {
+            newRecipe.setId(id);
+            recipeRepository.save(newRecipe);
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
     }
 
     public List<Recipe> findAllByCategory(String category) {
